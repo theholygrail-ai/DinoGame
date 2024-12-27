d00c676f38097e6a1b91bb8ac3f256c6124f1660
theholygrail-ai <fxholygrailsa@gmail.com>
2024-12-27 14:13:24 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DinoGame from './DinoGame';
+
+function App() {
+  return (
+    <div className="App">
+      <DinoGame />
+    </div>
+  );
+}
+
+export default App;
